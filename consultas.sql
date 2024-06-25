e9a1b91d8d8dc74458c9209c5de5b6ea7640a53c
RupnikF <francorupnik@gmail.com>
2024-06-25 17:46:21 -0300

    changes querys

--- consultas.sql
+++ consultas.sql
@@ -3,12 +3,19 @@
  ¿Existe correlación entre el PBI per cápita de un país y su tasa de mortalidad?
  Por año y territorio, el PBI per cápita y la tasa de mortalidad
  */
-SELECT pbi, deaths
-FROM year_data;
+WITH year_deaths_covid AS (
+    SELECT country_key, year, sum(deaths) as covid_deaths
+    FROM daily_data NATURAL JOIN time
+    GROUP BY country_key, year
+)
+SELECT pbi, deaths, covid_deaths
+FROM year_data NATURAL JOIN year_deaths_covid
+ORDER BY  pbi;
 
 
 /*
-(CAMBIADA) Determinar si existe un cambio de mortalidad frente a diferentes estaciones. Para un mes, año y territorio, estación de cada país y la tasa de mortalidad
+Determinar si existe un cambio de mortalidad frente a diferentes estaciones.
+Para cada estación la tasa de mortalidad
 */
 SELECT avg(deaths),avg(infections),season
 FROM daily_data
@@ -110,19 +117,20 @@ ORDER BY delta;
 Discretizar los tipos de regímenes, ponerle una clasificación
 Para un país, clasificado en vacunación obligatoria y opcional, la tasa de mortalidad anual
 */
-WITH stringency_resumida AS (
-    SELECT deaths, infections,
+WITH country_stringency AS (
+    SELECT country_key,
     CASE
-        WHEN policy_level BETWEEN 1 AND 20 THEN 1
-        WHEN policy_level BETWEEN 21 AND 40 THEN 2
-        WHEN policy_level BETWEEN 41 AND 60 THEN 3
-        WHEN policy_level BETWEEN 61 AND 80 THEN 4
-        WHEN policy_level BETWEEN 81 AND 100 THEN 5
+        WHEN avg(policy_level) BETWEEN 1 AND 20 THEN 1
+        WHEN avg(policy_level) BETWEEN 21 AND 40 THEN 2
+        WHEN avg(policy_level) BETWEEN 41 AND 60 THEN 3
+        WHEN avg(policy_level) BETWEEN 61 AND 80 THEN 4
+        WHEN avg(policy_level) BETWEEN 81 AND 100 THEN 5
         ELSE 0 -- In case policy_level is outside the expected range
         END AS policy_group
-    FROM daily_data NATURAL JOIN time)
+    FROM daily_data
+    GROUP BY country_key)
 SELECT policy_group, avg(deaths) as avg_deaths, avg(infections) as avg_infections
-FROM stringency_resumida
+FROM country_stringency NATURAL JOIN daily_data
 GROUP BY policy_group;
 
 
@@ -130,4 +138,78 @@ GROUP BY policy_group;
 ¿Qué estrategias de vacunación vieron una mejora sustancial (en comparación con otras) después de 3 meses? ¿Y después de 6 meses? ¿Y año?
 Mismo que antes ponerle una categorizar cada país
 */
+WITH country_stringency_month AS (
+    SELECT country_key, year, monthnumber, sum(infections) as infections,
+           CASE
+               WHEN avg(policy_level) BETWEEN 1 AND 20 THEN 1
+               WHEN avg(policy_level) BETWEEN 21 AND 40 THEN 2
+               WHEN avg(policy_level) BETWEEN 41 AND 60 THEN 3
+               WHEN avg(policy_level) BETWEEN 61 AND 80 THEN 4
+               WHEN avg(policy_level) BETWEEN 81 AND 100 THEN 5
+               ELSE 0 -- In case policy_level is outside the expected range
+               END AS policy_group
+    FROM daily_data NATURAL JOIN time
+    GROUP BY year, monthnumber, country_key),
+compare_infections AS (
+    SELECT country.name, year, monthnumber, infections,
+        LAG(infections,3) OVER (PARTITION BY country.name ORDER BY year, monthnumber) as month_3,
+        LAG(infections,6) OVER (PARTITION BY country.name ORDER BY year, monthnumber) as month_6,
+        LAG(infections,12) OVER (PARTITION BY country.name ORDER BY year, monthnumber) as month_12
+    FROM country_stringency_month NATURAL JOIN country)
+SELECT name, year, monthnumber, infections, (infections - month_3) as delta_3, (infections - month_6) as delta_6, (infections - month_12) as delta_12
+FROM compare_infections
+ORDER BY delta_3;
+
+
+
+/*
+¿Un aumento en el sistema de salud mejora las tasas de mortalidad e infección? Por año y territorio,
+la tasa de mortalidad e infección per cápita y la diferencia de presupuesto en el sistema de salud(% del PBI) con el año anterior
+*/
+
+SELECT deaths, health_budget, country.name as country_name, LAG(deaths,1) OVER (PARTITION BY country.name ORDER BY year) as prev_year_deaths, year
+FROM year_data NATURAL JOIN country
+ORDER BY country, year;
+
+
+/*
+¿Qué meses fueron los peores en tasa de mortalidad por región? Por mes del año y región, la tasa de mortalidad mensual
+*/
+WITH monthly_deaths AS (SELECT sum(deaths) as monthly_deaths_by_country, region, country.name, monthnumber, year
+                        FROM daily_data natural join time natural join country natural join region
+                        GROUP BY monthnumber, country.name, year),
+    almost_there AS (SELECT AVG(monthly_deaths_by_country) as deaths, region, monthnumber as month, year
+                     FROM monthly_deaths
+                     GROUP BY region,monthnumber, year
+                     ORDER BY deaths)
+SELECT *, rank() OVER (PARTITION BY region ORDER BY deaths)
+FROM almost_there;
+
+
+/*
+¿Frente a qué temperaturas se registró un aumento en los casos?
+*/
+
+SELECT season, sum(infections) as total_casos, country.name
+FROM daily_data NATURAL JOIN country
+GROUP BY season, country.name
+ORDER BY country.name, total_casos;
+
+
+/*
+¿Existe un aumento de casos para fechas festivas?
+SUPONGO LAG DE 20 días EXCELENTE
+*/
+WITH prev_15_data AS(
+    SELECT time_key, infections, lag(infections, 20) OVER (PARTITION BY country_key ORDER BY year, monthnumber, day) as post_20
+    FROM daily_data NATURAL JOIN time
+)
+SELECT time_key, infections, post_20, (infections - post_20) AS delta_20
+FROM prev_15_data
+WHERE (TO_CHAR(time_key, 'MM-DD') = '12-25' -- navidad
+    OR TO_CHAR(time_key, 'MM-DD') = '01-01' -- año nuevo
+    OR TO_CHAR(time_key, 'MM-DD') = '25-05' -- 25 de mayo
+    OR TO_CHAR(time_key, 'MM-DD') = '01-05') -- Día del trabajado
+
+
 
